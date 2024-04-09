16d9aa5e48f44f29dfde97018aad8e029a9f6b81
Maquet Dorian <takenzmr@gmail.com>
2024-04-09 20:35:11 +0200

    Update Script

---
+++ script.js
@@ -0,0 +1,26 @@
+function createGalery () {
+    const container = document.querySelector("#photos .container");
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    fetch("data/galery.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((item, index) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <img src="./images/${item.image}">
+            `;
+            row.appendChild(card);
+
+            if ((index + 1) % 4 == 0 || index == data.length - 1) {
+                container.appendChild(row);
+                row = document.createElement("div");
+                row.classList.add("row");
+            }
+        });
+    });
+}
+
+createGalery()
